2bd17eb02e364595ec9ec6e271d4748183b39e05
demonzyj56 <demonzyj56@gmail.com>
2018-10-05 21:44:38 +0800

    Change to reduction kernel

--- extensions/farthest_point_sample_cuda.cpp
+++ extensions/farthest_point_sample_cuda.cpp
@@ -6,6 +6,7 @@
 
 void fps_cuda_forward(at::Tensor pcs, at::Tensor out);
 void fps_cuda_forward_one_pass(at::Tensor pcs, at::Tensor out);
+void fps_cuda_1(at::Tensor pcs, at::Tensor out);
 
 void fps_forward(at::Tensor pcs, at::Tensor out) {
     CHECK_INPUT(pcs);
@@ -19,8 +20,15 @@ void fps_forward_one_pass(at::Tensor pcs, at::Tensor out) {
     fps_cuda_forward_one_pass(pcs, out);
 }
 
+void fps1(at::Tensor pcs, at::Tensor out) {
+    CHECK_INPUT(pcs);
+    CHECK_INPUT(out);
+    fps_cuda_1(pcs, out);
+}
+
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
     m.def("forward", &fps_forward, "CUDA forward routine for farthest point sampling");
     m.def("forward2", &fps_forward_one_pass, 
             "CUDA forward routine for farthest point sampling (simple implementation)");
+    m.def("forward3", &fps1, "CUDA forward routine with reduction kernel.");
 }
