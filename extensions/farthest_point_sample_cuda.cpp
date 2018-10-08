19555d92cffdf3a620723422f6615b1c5b824387
demonzyj56 <demonzyj56@gmail.com>
2018-10-08 20:24:37 +0800

    Correct fps impl.

--- extensions/farthest_point_sample_cuda.cpp
+++ extensions/farthest_point_sample_cuda.cpp
@@ -8,6 +8,7 @@ void fps_cuda_forward(at::Tensor pcs, at::Tensor out);
 void fps_cuda_forward_one_pass(at::Tensor pcs, at::Tensor out);
 void fps_cuda_1(at::Tensor pcs, at::Tensor out);
 void fps_cuda_2(at::Tensor pcs, at::Tensor out);
+void fps_cuda_correct(at::Tensor pcs, at::Tensor out);
 
 void fps_forward(at::Tensor pcs, at::Tensor out) {
     CHECK_INPUT(pcs);
@@ -33,10 +34,17 @@ void fps2(at::Tensor pcs, at::Tensor out) {
     fps_cuda_2(pcs, out);
 }
 
+void fps3(at::Tensor pcs, at::Tensor out) {
+    CHECK_INPUT(pcs);
+    CHECK_INPUT(out);
+    fps_cuda_correct(pcs, out);
+}
+
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
     m.def("forward", &fps_forward, "CUDA forward routine for farthest point sampling");
     m.def("forward2", &fps_forward_one_pass, 
             "CUDA forward routine for farthest point sampling (simple implementation)");
     m.def("forward3", &fps1, "CUDA forward routine with reduction kernel.");
     m.def("forward4", &fps2, "CUDA forward routine with fully unrolled reduction kernel.");
+    m.def("forward5", &fps3, "CUDA forward routine with reduction kernel.");
 }
--- extensions/farthest_point_sample_cuda_kernel.cu
+++ extensions/farthest_point_sample_cuda_kernel.cu
@@ -483,3 +483,79 @@ void fps_cuda_2(at::Tensor pcs, at::Tensor out) {
             break;
     }
 }
+
+// We assume for now that num_points <= blockDim.x.
+// Fix some bugs.
+template <typename scalar_t>
+__global__ void fps_kernel_correct(int batch_size, int num_points, int num_centroids, const scalar_t *pcs,
+        int64_t *centroid_idx) {
+    // shared memory
+    extern __shared__ float sdata[];
+    float *dists = sdata;
+    int64_t *max_idx = (int64_t *)&dists[blockDim.x];
+    bool *sampled = (bool *)&max_idx[blockDim.x];
+
+    int64_t batch = blockIdx.x;
+    int64_t tid = threadIdx.x;
+
+    // loop over all centroids
+    centroid_idx[batch*num_centroids] = 0;
+    dists[tid] = 1e38;
+    sampled[tid] = (tid == 0) ? true : false;
+    __syncthreads();
+    for (int64_t i = 1; i < num_centroids; ++i) {
+        int64_t prev_idx = centroid_idx[batch*num_centroids+i-1];
+        if (tid < num_points) {
+            scalar_t x1 = pcs[(batch*3+0)*num_points+prev_idx];
+            scalar_t x2 = pcs[(batch*3+1)*num_points+prev_idx];
+            scalar_t x3 = pcs[(batch*3+2)*num_points+prev_idx];
+            scalar_t y1 = pcs[(batch*3+0)*num_points+tid];
+            scalar_t y2 = pcs[(batch*3+1)*num_points+tid];
+            scalar_t y3 = pcs[(batch*3+2)*num_points+tid];
+            scalar_t d = static_cast<float>((x1-y1)*(x1-y1) + (x2-y2)*(x2-y2) + (x3-y3)*(x3-y3));
+            if (d < dists[tid]) {
+                dists[tid] = d;
+            }
+        } 
+        max_idx[tid] = tid;
+        __syncthreads();
+
+        // do reduction!
+        for (int64_t s = num_points / 2; s > 0; s >>= 1) {
+            if (tid < s) {
+                // four cases depending whether each point is sampled or not.
+                if (!sampled[max_idx[tid+s]]) {
+                    if (sampled[max_idx[tid]] || (dists[tid] < dists[tid+s])) {
+                        dists[tid] = dists[tid+s];
+                        max_idx[tid] = max_idx[tid+s];
+                    }
+                }
+            }
+            __syncthreads();
+        }
+
+        // write result to global memory
+        if (tid == 0) {
+            centroid_idx[batch*num_centroids+i] = max_idx[0];
+        }
+
+        // update sampled record
+        if (tid == max_idx[0]) {
+            sampled[max_idx[0]] = true;
+        }
+        __syncthreads();
+
+    }
+}
+
+void fps_cuda_correct(at::Tensor pcs, at::Tensor out) {
+    int batch_size = pcs.size(0);
+    int num_centroids = out.size(1);
+    int num_points = pcs.size(2);
+    int threads = 1024;
+    int smem_size = (sizeof(float) + sizeof(int64_t) + sizeof(bool)) * threads;
+    AT_DISPATCH_FLOATING_TYPES(pcs.type(), "fps1_kernel", ([&] {
+        fps_kernel_correct<<<batch_size, threads, smem_size>>>(batch_size, num_points, num_centroids,
+                pcs.data<scalar_t>(), out.data<int64_t>());
+    }));
+}
